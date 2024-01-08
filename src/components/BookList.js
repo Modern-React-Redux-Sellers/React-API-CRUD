053059dbd5f0ff546b13505c8943a4ccc1bc298f
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-01-07 21:43:34 -0600

    began work on using context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,17 @@
 import BookShow from "./BookShow";
+import {useContext} from "react";
+import BooksContext from "../context/books";
 
 const BookList = ({books, onDelete, onEdit}) => {
+    const contextVal = useContext(BooksContext);
+
     const renderBooks = books.map((book) => {
         return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit}/>
     })
 
     return (
         <div className="book-list">
+            {contextVal}
             {renderBooks}
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css';
+import BooksContext from "./context/books";
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+    <BooksContext.Provider value={5}>
+        <App />
+    </BooksContext.Provider>
+);
